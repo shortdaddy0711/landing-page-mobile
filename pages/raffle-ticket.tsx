7132ae6526ec687f79b2ff5515afb53887973625
shortdaddy0711 <sodoatm@gmail.com>
2024-10-25 11:14:07 -0700

    add loading state during google sheet fetching

--- pages/raffle-ticket.tsx
+++ pages/raffle-ticket.tsx
@@ -6,8 +6,6 @@ export default function RaffleTicket() {
     const name = searchParams.get('name');
     const raffle = searchParams.get('raffleNumber');
 
-    console.log(name, raffle);
-
     return (
         <div className='w-full flex flex-col items-center justify-start min-h-screen text-slate-900  text-center bg-violet-700 p-2'>
             <h2 className='text-3xl font-bold m-4 text-yellow-400'>Welcome to MVCYM!</h2>
